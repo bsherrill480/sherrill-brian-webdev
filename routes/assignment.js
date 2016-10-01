b16ae938d49f718b196ba5a1fb0f0b8eabd7e38b
brian <bdsherrill9@gmail.com>
2016-10-01 04:02:36 -0700

    Saving

--- routes/assignment.js
+++ routes/assignment.js
@@ -9,6 +9,8 @@ router.get('/register.html', function (req, res, next) {
     res.render('assignment/user_pages/register');
 });
 
+// WEBSITE PAGES
+
 router.get('/website-list.html', function (req, res, next) {
     res.render('assignment/website_pages/website_list');
 });
@@ -20,5 +22,21 @@ router.get('/website-edit.html', function (req, res, next) {
 router.get('/website-new.html', function (req, res, next) {
     res.render('assignment/website_pages/website_new');
 });
+
+// PAGE PAGES
+
+router.get('/page-list.html', function (req, res, next) {
+    res.render('assignment/page_pages/page_list');
+});
+
+router.get('/page-edit.html', function (req, res, next) {
+    res.render('assignment/page_pages/page_edit');
+});
+
+router.get('/page-new.html', function (req, res, next) {
+    res.render('assignment/page_pages/page_new');
+});
+
+
 
 module.exports = router;
